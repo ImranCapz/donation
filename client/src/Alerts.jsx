a2205118889661ecceed56ff09a3ab06a360b16a
Imran Nazeer S <149575738+ImranNazeerS@users.noreply.github.com>
2024-06-12 18:00:53 +0530

    new commit

--- client/src/Alerts.jsx
+++ client/src/Alerts.jsx
@@ -1,13 +1,12 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import axios from "axios";
 
 function Alerts() {
-  const [type, setType] = useState("donation");
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("Received from");
   const [name, setName] = useState("");
   const [result, setResult] = useState("");
-  const navigate = useNavigate();
+  const [amount, setAmount] = useState("");
+  const [currency, setCurrency] = useState("INR");
 
   const getAccessToken = () => {
     window.open("/server/donation/auth", "_blank");
@@ -15,19 +14,12 @@ function Alerts() {
 
   const sendAlert = async () => {
     try {
-      const accessToken = "x5fRtjYEbeisH70P1tLUgSLXWoizsCW07vDeYmiG";
-      const name = "Test";
-      const message = "Test message";
-      const amount = 100;
-      const currency = "USD";
       const response = await axios.post("/server/donation/sendalert", {
-        accessToken,
         name,
         message,
         amount,
         currency,
       });
-
       if (response.data.success) {
         setResult("Alert sent successfully: " + JSON.stringify(response.data));
       } else {
@@ -44,10 +36,38 @@ function Alerts() {
   };
 
   return (
-    <div className="App">
+    <div className="flex flex-col w-60 mx-auto ">
       <h1>Send Streamlabs Alert</h1>
       <button onClick={getAccessToken}>Get Access Token</button>
-      <button onClick={sendAlert}>Send Alert</button>
+      <div className="flex flex-col">
+        <label className="text-sm font-semibold text-main">Name:</label>
+        <input
+          type="text"
+          value={name}
+          className="input border-2 p-2 rounded-lg  focus:ring-0 md:w-60"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label className="text-sm font-semibold text-main">Message:</label>
+        <input
+          type="text"
+          value={message}
+          className="input border-2 p-2 rounded-lg  focus:ring-0 md:w-60"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <label className="text-sm font-semibold text-main">Amount</label>
+        <input
+          type="number"
+          value={amount}
+          className="border-2 p-2 rounded-lg  focus:ring-0 md:w-60"
+          onChange={(e) => setAmount(e.target.value)}
+        />
+      </div>
+      <button
+        className="p-3 bg-sky-200  btn-color rounded-lg mt-16 font-semibold rounded=lg uppercase hover:opacity-95 disabled:opacity-60 transition ease-in-out duration-300"
+        onClick={sendAlert}
+      >
+        Send Alert
+      </button>
       <p>{result}</p>
     </div>
   );
--- server/controllers/donation.controller.js
+++ server/controllers/donation.controller.js
@@ -1,35 +1,30 @@
 import axios from "axios";
 
-export const getauth = async (req, res) => {
-  const authUrl =
-    "https://www.streamlabs.com/api/v1.0/authorize?client_id=7ZL9RJRixjdyM7TZE6PmbM4Om8yP6n4M5r41qL1R&redirect_uri=http://localhost:5000/callback&response_type=code&scope=donations.read+donations.create";
-  res.redirect(authUrl);
-};
 
-export const callback = async (req, res) => {
-  const code = req.query.code;
-  console.log(`Received code: ${code}`);
-  try {
-    const response = await axios.post("https://streamlabs.com/api/v1.0/token", {
-      grant_type: "authorization_code",
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      code: code,
-      redirect_uri: process.env.REDIRECT_URI,
-    });
-    const accessToken = response.data.access_token;
-    console.log(`Received access token: ${accessToken}`);
-    res.json({ accessToken });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: error.response ? error.response.data : error.message });
-  }
-};
+// export const callback = async (req, res) => {
+//   const code = req.query.code;
+//   console.log(`Received code: ${code}`);
+//   try {
+//     const response = await axios.post("https://streamlabs.com/api/v1.0/token", {
+//       grant_type: "authorization_code",
+//       client_id: process.env.CLIENT_ID,
+//       client_secret: process.env.CLIENT_SECRET,
+//       code: code,
+//       redirect_uri: process.env.REDIRECT_URI,
+//     });
+//     const accessToken = response.data.access_token;
+//     console.log(`Received access token: ${accessToken}`);
+//     res.json({ accessToken });
+//   } catch (error) {
+//     res
+//       .status(500)
+//       .json({ error: error.response ? error.response.data : error.message });
+//   }
+// };
 
 export const sendalert = async (req, res) => {
   const { name, message, amount, currency } = req.body;
-  const accessToken = req.body.accessToken;
+  const accessToken = process.env.ACCESS_TOKEN;
   const alertData = {
     message: message,
     name: name,
